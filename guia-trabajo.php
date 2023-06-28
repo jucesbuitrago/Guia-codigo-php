b2ae756077ac26c0b640e8ab26582ca152d050ab
Julio Buitrago <cesarbuitrago78@gmail.com>
2023-06-27 19:05:52 -0500

    Uso de la codificación UTF-8

---
+++ guia-trabajo.php
@@ -0,0 +1,24 @@
+/**
+    Guía de Código
+
+    Introducción
+
+    Una guía de estilo de un lenguaje de programación es un conjunto de recomendaciones sobre la forma de dar formato a los programas. El interés de utilizar un estilo específico es facilitar la re utilización de código y la detección de errores. Existen muchos estilos de programación y no se puede decir que uno sea mejor que otro, pero sí que es conveniente adoptar algún estilo determinado y utilizarlo de forma consistente.
+
+    Esta guia se basa en las recomendaciones hechas por el grupo PHP-FIG (PHP framework Interop Group)[1] en el cual participan los autores de los frameworks PHP mas populares.
+*/
+
+/**Guia
+    Los archivos DEBEN emplear solamente la codificacion UTF-8 sin BOOM para el código de PHP.
+
+    --------NO USAR---------
+    ï»¿<?php
+        // Código PHP aquí
+    ?>
+
+    -----------------USO NORMAL-------------------
+    <?php
+        // Código PHP aquí
+    ?>
+
+*/
